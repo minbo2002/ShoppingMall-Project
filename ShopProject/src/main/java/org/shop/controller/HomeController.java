c212f6b7710712223f9904fa6c9032650d13794b
minbo2002 <minbo2002@naver.com>
2022-10-17 22:24:41 +0900

    프로젝트 첫 생성 및 환경설정

---
+++ ShopProject/src/main/java/org/shop/controller/HomeController.java
@@ -0,0 +1,27 @@
+package org.shop.controller;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping
+	public String home(Locale locale, Model model) {
+		
+		LocalDateTime now = LocalDateTime.now();
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 (E) a h시 m분 s초");
+		String formattedNow = now.format(formatter);
+		
+		model.addAttribute("serverTime", formattedNow);
+		
+		return "home";
+	}
+}
---
+++ ShopProject/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,12 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+	<h1>방문을 환영합니다</h1>
+	<P>${serverTime}</P>
+</body>
+
+</html>
